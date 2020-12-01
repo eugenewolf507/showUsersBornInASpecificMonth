e8f7af1dc5ee3f7a70b3814f3cbd5fb4cd1e3afd
Eugene Volkov <eugenewolf507@gmail.com>
2020-12-01 09:26:48 +0200

    fix GHpages link

--- build/service-worker.js
+++ build/service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/showUsersBornInASpecificMonth/build/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
